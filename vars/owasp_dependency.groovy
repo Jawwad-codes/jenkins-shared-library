b50b6de6e18c7a3e57406d9bf624457662a45c65
jawwad-codes <jawwadnadeemsadiq@gmail.com>
2026-09-21 09:43:24 +0500

    Fix formatting of additionalArguments in owasp_dependency function for consistency

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,6 +1,6 @@
 def call(){
     withCredentials([string(credentialsId: 'NVD-KEY', variable: 'NVD_API_KEY')]) {
-        dependencyCheck additionalArguments: "--scan ./ --format XML --nvdApiKey ${NVD_API_KEY}", odcInstallation: 'OWASP'
+        dependencyCheck additionalArguments: '--scan ./ --format XML --nvdApiKey ${NVD_API_KEY}', odcInstallation: 'OWASP'
     }
     dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
