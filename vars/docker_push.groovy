49a62c72f22f1670bf1dd01d731faead7da9e3dc
jawwad-codes <jawwadnadeemsadiq@gmail.com>
2026-09-22 10:32:41 +0500

    Fix docker image push command to include dockerhubUsername in image path

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,7 +4,7 @@ def call(Map config = [:]) {
     def dockerhubUsername = config.dockerhubUsername ?: error("Docker Hub username is required")
     def credentials = config.credentials ?: 'docker-hub-credentials'
     
-    echo "Pushing Docker image: ${imageName}:${imageTag}"
+    echo "Pushing Docker image: ${dockerhubUsername}/${imageName}:${imageTag}"
     
     withCredentials([usernamePassword(
         credentialsId: credentials,
@@ -13,8 +13,8 @@ def call(Map config = [:]) {
     )]) {
         sh """
             echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
-            docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
+            docker push ${dockerhubUsername}/${imageName}:${imageTag}
+            docker push ${dockerhubUsername}/${imageName}:latest
         """
     }
 }
