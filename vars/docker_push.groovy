67829332874a8d9e4667213fd67d283bffbadc6c
jawwad-codes <jawwadnadeemsadiq@gmail.com>
2026-09-22 10:21:49 +0500

    Add dockerhubUsername parameter to docker_push function for improved error handling

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]) {
     def imageName = config.imageName ?: error("Image name is required")
     def imageTag = config.imageTag ?: 'latest'
+    def dockerhubUsername = config.dockerhubUsername ?: error("Docker Hub username is required")
     def credentials = config.credentials ?: 'docker-hub-credentials'
     
     echo "Pushing Docker image: ${imageName}:${imageTag}"
