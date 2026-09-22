930a6a7bad5beb1821f9b8f69147be73837f3128
jawwad-codes <jawwadnadeemsadiq@gmail.com>
2026-09-22 08:59:28 +0500

    Add dockerhubUsername parameter to docker_build function for improved error handling

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,6 +1,7 @@
 def call (Map config = [:]) {
     def imageName = config.imageName ?: error("imageName is required")
     def imageTag = config.imageTag ?: error("imageTag is required")
+    def dockerhubUsername = config.dockerhubUsername ?: error("dockerhubUsername is required")
     def dockerfile = config.dockerfile?: 'Dockerfile'
     def contextDir = config.contextDir?: '.'
     echo "Building docker image: ${imageName}:${imageTag} using Dockerfile: ${dockerfile} in context: ${contextDir}"
